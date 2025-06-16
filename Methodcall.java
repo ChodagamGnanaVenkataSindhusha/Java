c67bee80e5d22736fc6172681925f1bc50738764
ChodagamGnanaVenkataSindhusha <sindhushachodagam689@gmail.com>
2025-06-17 05:18:42 +0530

    Create Methodcall.java

---
+++ Methodcall.java
@@ -0,0 +1,13 @@
+public class PrintMyName
+{
+    public void printName()
+    {
+        System.out.println("My name is Sindhu");
+    }
+public static void main(String[] args)
+{
+        PrintMyName obj = new PrintMyName();
+        obj.printName();
+}
+}
+
