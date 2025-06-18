10221bdb277059311163f5a3d08c4a1ef86cd3da
ChodagamGnanaVenkataSindhusha <sindhushachodagam689@gmail.com>
2025-06-18 07:04:41 +0530

    Create DoWhile 1.java

---
+++ DoWhile 1.java
@@ -0,0 +1,13 @@
+public class Dowhile1 
+{
+    public static void main(String[] args) 
+    {
+        int i = 1;
+        do 
+        {
+            System.out.print(i + " ");
+            i++;
+        } while(i <= 10);
+    }
+}
+
