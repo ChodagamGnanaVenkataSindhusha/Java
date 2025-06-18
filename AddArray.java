43fcfa71220b637fe556a6223bc06d3ba8a30837
ChodagamGnanaVenkataSindhusha <sindhushachodagam689@gmail.com>
2025-06-18 17:04:57 +0530

    AddArray.java

--- AddArray.java
+++ AddArray.java
@@ -2,7 +2,7 @@ public class AddArray
 {
     public static void main(String[] args) 
     {
-        int[] arr = {5, 10, 15, 20};  
+        int arr[] = {5, 10, 15, 20};  
         int sum = 0;
         for (int i = 0; i < arr.length; i++) 
         {
