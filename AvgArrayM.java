e4655a515acc9477de0361606e5d941de743492a
ChodagamGnanaVenkataSindhusha <sindhushachodagam689@gmail.com>
2025-06-19 06:26:15 +0530

    Create AvgArrayM.java

---
+++ AvgArrayM.java
@@ -0,0 +1,18 @@
+public class AvgArrayM
+{
+    public static double findAverage()
+    {
+        int[] arr = {10, 20, 30, 40, 50};
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) 
+        {
+            sum = sum + arr[i];
+        }
+        return (double) sum / arr.length;
+    }
+    public static void main(String[] args) 
+    {
+        double avg = findAverage();
+        System.out.println("Average = " + avg);
+    }
+}
