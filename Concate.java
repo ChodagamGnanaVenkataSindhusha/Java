c66f9263d266c4c99df79af77c86d2fb5ed202de
ChodagamGnanaVenkataSindhusha <sindhushachodagam689@gmail.com>
2025-06-21 14:27:38 +0530

    Create Concate.java

---
+++ Concate.java
@@ -0,0 +1,17 @@
+//2 Immutable [ plus ,concat]
+import java.util.Scanner;
+class Concate
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter first string:");
+String s1=s.nextLine();
+System.out.println("Enter first string:");
+String s2=s.nextLine();
+String result1=s1+s2;                         //we have to declare 'string'
+String result2=s1.concat(s2);
+System.out.println("using plus:"+ result1);
+System.out.println("using concat:"+result2);
+}
+}
