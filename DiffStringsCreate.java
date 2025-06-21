ea62077641b5bf91de184aef2476c051e188e887
ChodagamGnanaVenkataSindhusha <sindhushachodagam689@gmail.com>
2025-06-21 14:11:49 +0530

    Create DiffStringsCreate.java

---
+++ DiffStringsCreate.java
@@ -0,0 +1,16 @@
+// 1    immutable
+class DiffStringsCreate
+{
+public static void main(String[] args)
+{
+String s1="Sindhu";                         //string literal
+String s2=new String("Chodagam"); //string object
+String s3=" ";                                  //empty
+char[]  s4={'c' , 's' ,'e'};                   //necessary to keep brackets to print array
+String s=new String(s4);                  //print array
+System.out.println(s1);
+System.out.println(s2);
+System.out.println(s3);
+System.out.println(s4);
+}
+}
