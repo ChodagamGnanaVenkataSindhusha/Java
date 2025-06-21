9acaa40f17c421d1816c9c220ee817a503602d51
ChodagamGnanaVenkataSindhusha <sindhushachodagam689@gmail.com>
2025-06-21 14:42:00 +0530

    Create StringLength.java

---
+++ StringLength.java
@@ -0,0 +1,17 @@
+//3   Immutable
+import java.util.Scanner;
+class StringLength
+{
+public static void main(String[] args)
+{
+Scanner s =new Scanner(System.in);
+System.out.println("Enter a string:");
+String s1=s.nextLine();
+int length=s1.length();                      //length "l" is small letter
+System.out.println("String length:"+length);
+}
+}
+o/p:
+Enter a string:
+OM NAMAH SIVAYYA
+String length:16
