9faf20427a6e2a61bffefb37f7604c8ad3ba98e2
ChodagamGnanaVenkataSindhusha <sindhushachodagam689@gmail.com>
2025-06-21 12:05:28 +0530

    Create String1.java

---
+++ String1.java
@@ -0,0 +1,18 @@
+class String1
+{
+public static void main (String[] args)
+{
+String s1="Sviet";
+String s2="Sviet";
+String s3="Sviet";
+String s4= new String("Sviet");
+String s5="SVIET";
+System.out.println(s1==s2);
+System.out.println(s1==s3);
+System.out.println(s1==s4);
+System.out.println(s1==s5);
+System.out.println(s4==s5);
+System.out.println(s1.equalsIgnoreCase(s4));
+System.out.println(s1.equalsIgnoreCase(s5));
+}
+}
