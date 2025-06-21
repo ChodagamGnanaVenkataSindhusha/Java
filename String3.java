e7667db126468a9bc3e68937cf96c9c405a0a289
ChodagamGnanaVenkataSindhusha <sindhushachodagam689@gmail.com>
2025-06-21 13:52:15 +0530

    Create String3.java

---
+++ String3.java
@@ -0,0 +1,12 @@
+class String3
+{
+public static void main(String[] args)
+{
+String s1="sviet";
+String s2="Sviet";
+s1=s1+s2;      // combining using plus
+String s6="sviet+sviet";
+System.out.println(s1==s6);
+System.out.println(s1);
+}
+}
