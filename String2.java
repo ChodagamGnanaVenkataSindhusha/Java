ac907f88ab8393af12b3c427858a96acfbe02a63
ChodagamGnanaVenkataSindhusha <sindhushachodagam689@gmail.com>
2025-06-21 13:41:01 +0530

    Create String2.java

---
+++ String2.java
@@ -0,0 +1,23 @@
+class String2
+{
+public static void main(String[] args)
+{
+String s1="sviet";
+String s2="sviet";String s3="Sviet";//stores in constant pool
+String s4="SVIET";
+String s5= new String("sviet");// stores in heap memory(object created)
+String s6=new String("SVIET");
+System.out.println(s1.equals(s2));//equal checks the content is same or not
+System.out.println(s1.equals(s3));
+System.out.println(s1.equals(s4));
+System.out.println(s1.equals(s5));
+System.out.println(s1.equals(s6));
+System.out.println(s5.equals(s6));
+System.out.println(s1==s2); //== checks the memory address is same or not
+System.out.println(s1==s3);
+System.out.println(s1==s4);
+System.out.println(s1==s5);//stores in differnt locations so false
+System.out.println(s1==s6);
+System.out.println(s5==s6);
+}
+}
