04813f30c70a0ba36d6782b4369c4049183df1d7
ChodagamGnanaVenkataSindhusha <sindhushachodagam689@gmail.com>
2025-06-21 15:08:21 +0530

    Create Substring.java

---
+++ Substring.java
@@ -0,0 +1,16 @@
+//4   Immutable
+class Substring
+{
+public static void main(String[] args)
+{
+String   str = "Believe in Sivayya";             
+String s1=str.substring(7);           //strats from 7 index and print up to end
+String s2=str.substring(0,10);      //starts from 0 and print upto 9   [substring 's ' should be small ]
+System.out.println(s1);               //count from 0 index ,including spaces
+System.out.println(s2);
+}
+}
+
+o/p:
+in Sivayya
+Believe in
